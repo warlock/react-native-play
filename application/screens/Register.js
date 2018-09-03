00356c946d67a6e5567ee041f6d517bc8a047f0f
Josep Subils <js@js.gl>
2018-09-03 11:42:52 +0200

    simplify register

--- application/screens/Register.js
+++ application/screens/Register.js
@@ -5,6 +5,7 @@ import AppButton from "../components/AppButton"
 import t from 'tcomb-form-native'
 import { registerValidation } from '../utils/validation'
 import { Card } from 'react-native-elements'
+import firebase from 'firebase/app'
 const Form = t.form.Form
 
 export default class Register extends Component {
@@ -13,11 +14,8 @@ export default class Register extends Component {
 
     this.state = {
       user: {
-        birthdate: '',
         password_clear: '',
         confirm_password: '',
-        first_name: '',
-        last_name: '',
         email: ''
       }
     }
@@ -27,34 +25,16 @@ export default class Register extends Component {
     })
 
     this.registerForm = t.struct({
-      first_name: registerValidation.first_name,
-      last_name: registerValidation.last_name,
       email: registerValidation.email,
-      birthdate: registerValidation.birthdate,
       password_clear: registerValidation.password_clear,
       confirm_password: this.samePassword
     })
 
     this.options = {
       fields: {
-        first_name: {
-          label: 'Nombre',
-          help: 'Introduce tu nombre',
-          autoCapitalize: 'words'
-        },
-        last_name: {
-          label: 'Apellidos',
-          help: 'Introduce tu apellido',
-          autoCapitalize: 'words'
-        },
         email: {
           label: 'Email',
           help: 'Introduce tu email'
-        },
-        birthdate: {
-          label: 'Fecha de nacimiento',
-          help: 'Introduce tu fecha de nacimiento',
-          error: 'Fecha incorrecta',
         },
         password_clear: {
           label: 'Contraseña',
@@ -83,27 +63,8 @@ export default class Register extends Component {
   register () {
     this.validate = this.refs.form.getValue()
     if (this.validate) {
-      fetch('', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          birthdate: this.validate.birthdate,
-          confirm_password: this.validate.confirm_password,
-          email: this.validate.email,
-          first_name: this.validate.first_name,
-          last_name: this.validate.last_name,
-          password_clear: this.validate.password_clear
-        })
-      })
-      .then(resp => resp.json())
-      .then(resp => {
-        console.log(resp)
-      })
-      .catch((error) => {
-        console.log(error)
+      firebase.auth().createUserWithEmailAndPassword(this.validate.email, this.validate.password).catch((error) => {
+        alert(error.message)
       })
     }
   }
